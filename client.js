f92c20f2aafe3670bd217c4fe43753caa155c2e9
Eppa Lea Turniawan <eppalea@gmail.com>
2021-02-10 22:03:16 +0000

    tested hard-coded Move: Up code with multiple lines of code or setTimeout/setInterval functions. code is commented is out since we don't want it to be hard coded

--- client.js
+++ client.js
@@ -1,4 +1,5 @@
 const net = require('net');
+let delayTime = 50;
 
 // Establishes connection with the game server
 const connect = function() {
@@ -16,6 +17,17 @@ const connect = function() {
   conn.on('connect', () => {  // callback function
     conn.write('Name: ELT'); // msg to send back to server 
   });  
+
+  
+  // conn.on('connect', () => {  // callback function
+  //   setInterval(() => { // setTimeout or setInterval will repeat the move
+  //     conn.write('Move: up'); // msg to send back to server 
+  //   }, delayTime += 50); 
+  //  }); 
+
+  // conn.on('connect', () => {  // callback function
+  //   conn.write('Move: up'); // msg to send back to server 
+  // }); 
 
   return conn;
 };
