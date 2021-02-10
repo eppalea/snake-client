a678ba35017df42dfc9171b185f215b0bdcba253
Eppa Lea Turniawan <eppalea@gmail.com>
2021-02-10 22:47:42 +0000

    moved the setupInput function to its own file. addad a require function to link the files. called the setupIntput() function at the bottom.

--- play.js
+++ play.js
@@ -1,22 +1,10 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input');
+
 console.log('Connecting ...');
 connect();
 
-// Setup User Interface
-// Specifically, so that we can handle user input via stdin
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-    
-  const handleUserInput = stdin.on('data', (key) => {
-      // \u0003 maps to ctrl+c input
-      if (key === '\u0003') {
-        process.exit();
-      }
-    });
-    
-    return stdin;
-  };
+setupInput();
+
+
 
