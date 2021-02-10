420518c7542ded0a01a26b6200b447d92aabfa25
Eppa Lea Turniawan <eppalea@gmail.com>
2021-02-10 22:39:50 +0000

    set up out stdin (standard input) by creating two functions. setupInput and handleUserInput. These help us establish our user interface (UI) where players can send keyboard commands to control the snake

--- play.js
+++ play.js
@@ -1,3 +1,22 @@
 const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
+
+// Setup User Interface
+// Specifically, so that we can handle user input via stdin
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+    
+  const handleUserInput = stdin.on('data', (key) => {
+      // \u0003 maps to ctrl+c input
+      if (key === '\u0003') {
+        process.exit();
+      }
+    });
+    
+    return stdin;
+  };
+
